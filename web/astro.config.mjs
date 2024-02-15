b5c1dc498f3ff60948e68e729281f20db1a96df2
appkalorder <appkalorder@gmail.com>
2024-02-14 22:37:49 -0500

    Adapter mode to middleware

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -6,7 +6,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
+  output: 'server',
   adapter: node({
     mode: "middleware"
   })
